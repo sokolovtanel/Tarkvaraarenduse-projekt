b371ea4f76f136be09c4c2c84b28e6aa9e9f968c
Tanel Sokolov <tanel.sokolov@vikk.ee>
2026-03-11 12:07:02 +0200

    YL3 valmis

--- Ülesanne_3/Ylesanne3.py
+++ Ülesanne_3/Ylesanne3.py
@@ -3,20 +3,41 @@ import sys
 pygame.init()
 
 # värvid
-Green = [153, 255, 153]
+Punane = [255, 0, 0]
+Roheline = [0, 255, 0]
+Sinine = [0, 0, 255]
+Must = [0, 0, 0]
+HRoheline = [144, 238, 144]
 
-#ekraani seaded
+
+# ekraani seaded
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Ülesanne 3")
-screen.fill(Green)
+screen.fill(HRoheline)
+
+# parameetrid
+ruudu_suurus = 20
+joone_laius = 2
+read = 24
+veerud = 32
+v2rv = Punane
+
+# ruudustiku joonistamise loop
+def ruudustik(surface):
+    # vertikaalsed jooned
+    for veerg in range(veerud + 1):
+        x = veerg * ruudu_suurus
+        pygame.draw.line(surface, v2rv, (x, 0), (x, read * ruudu_suurus), joone_laius)
+    # horisontaalsed jooned
+    for rida in range(read + 1):
+        y = rida * ruudu_suurus
+        pygame.draw.line(surface, v2rv, (0, y), (veerud * ruudu_suurus, y), joone_laius)
+
+# joonistame ruudustiku
+ruudustik(screen)
+
 # ekraani värskendamine
 pygame.display.flip()
-
-
-
-
-
-
 
 # hoiame mängu elus (ilma selleta viskab kohe kinni)
 running = True
