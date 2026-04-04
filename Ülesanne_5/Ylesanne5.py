bd3fb7c0e6d2a7cf2c42e9ea3bf7bf7899e0082e
Tanel Sokolov <tanel.sokolov@vikk.ee>
2026-04-04 13:24:46 +0300

    v.2 lisatud alus, collision ja skoor

--- Ülesanne_5/Ylesanne5.py
+++ Ülesanne_5/Ylesanne5.py
@@ -10,17 +10,27 @@ screen = pygame.display.set_mode((screenX, screenY))
 clock = pygame.time.Clock()
 pygame.display.set_caption("PingPong")
 
+# definitsioonid
+skoor = 0
+
 # pildid
 pall = pygame.image.load("img/ball-1.png")
 alus = pygame.image.load("img/pad.png")
+pall = pygame.transform.scale(pall, (20, 20)) #laius, kõrgus
+alus = pygame.transform.scale(alus, (120, 20))
 
 # rect pallile
 pall_rect = pall.get_rect()
 pall_rect.center = (screenX / 2, screenY / 2)
 
+# rect alusele
+alus_rect = alus.get_rect()
+alus_rect.center= (screenX / 2, screenY / 1.5)
+
 # kiirused
 kiirusX = 3
 kiirusY = 3
+alusKiirus = 3
 
 running = True
 while running:
@@ -33,6 +43,9 @@ while running:
     pall_rect.x += kiirusX
     pall_rect.y += kiirusY
 
+    #liigutame alust
+    alus_rect.x += alusKiirus
+
     # pallipõrgatus
     if pall_rect.left < 0 or pall_rect.right >= screenX:
         kiirusX *= -1
@@ -40,33 +53,36 @@ while running:
     if pall_rect.top < 0 or pall_rect.bottom >= screenY:
         kiirusY *= -1
 
+    # aluse põrkamine
+    if alus_rect.left < 0 or alus_rect.right >= screenX:
+        alusKiirus *= -1
+
+    # veidra liikumise parandamiseks kontrollime kiirust ka
+    if pall_rect.colliderect(alus_rect) and kiirusY > 0:
+        # pall tuleb ülevalt (+1 skoor)
+        if pall_rect.bottom <= alus_rect.top + 10:
+            kiirusY *= -1
+            skoor += 1
+        # pall tuleb küljelt
+        else:
+            kiirusX *= -1
+
+    # kui pall puudutab põhja -1 skoorist
+    if pall_rect.bottom >= screenY:
+        skoor += -1
+
+    # kuvame tausta ja pildid
     screen.fill([153, 204, 255])
     screen.blit(pall, pall_rect)
+    screen.blit(alus, alus_rect)
+
+    # kuvame skoori
+    font = pygame.font.Font(None, 30)
+    text = font.render("Skoor: "+str(skoor), True, (255, 255, 255))
+    screen.blit(text, (10, 10))
 
     # ekraanivärskendus
     clock.tick(fps)
     pygame.display.flip()
 
 pygame.quit()
-
-
-
-
-
-'''
-*Lisa ja animeeri pall
-palli suurus 20×20
-*pall liigub sinu valitud kiirusega
-*pall põrkub seintest tagasi
-Lisa ja animeeri alus
-aluse suurus 120×20
-aluse y-koordinaat on keskkohast allpool. Näiteks screenY/1.5
-alus liigub vasakule/paremale (vahetab suunda, kui puudub seinu)
-Kokkupõrke tuvastamine
-kui pall puutub alust siis muudab suunda.
-kui pall käitub kokkupuutel alusega imelikult, siis lisa tingimusse kontroll, et palli y-suund oleks suurem kui null (ballSpeedY > 0)
-Boonus
-kui pall puudub alumist äärt, siis saab mängija negatiivse punkti
-kui pall puutub alust, siis saab positiivse punkti
-kuva tulemus mängu ülemises nurgas
-'''
